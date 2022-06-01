b4ba625b4caf9dbcbbe1f71cbe0ac9964bc97313
Amit Rana <amitrana@uminoguru.com>
2022-06-01 11:19:51 +0530

    users and members

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ const serviceAccount = require("./key.json");
 var MongoClient = require("mongodb").MongoClient;
 
 let fsb = new firestoreBackup(serviceAccount, "https://air-sniper.firebaseio.com");
-
 const client = new MongoClient(mongoConnectionUri);
 
-const excluded = ["devices", "wifiErrors"];
-const collections = [
-  "alertConditions",
-  "alerts",
-  "devices",
-  "firmware",
-  "groups",
-  "notification_logs",
-  "notifications",
-  "notifications_items",
-  "operatingManuals",
-  "org_members",
-  "orgs",
-  "schedule_items",
-  "schedules",
-  "socketLogs",
-  "sockets",
-  "terms_of_service",
-  "terms_of_service_compliance",
-  "users",
-  "wifiErrors",
-];
+const cNames = {
+  alertConditions: "alertConditions",
+  alerts: "alerts",
+  devices: "devices",
+  firmware: "firmware",
+  groups: "groups",
+  notification_logs: "notification_logs",
+  notifications: "notifications",
+  notifications_items: "notifications_items",
+  operatingManuals: "operatingManuals",
+  org_members: "org_members",
+  orgs: "orgs",
+  schedule_items: "schedule_items",
+  schedules: "schedules",
+  socketLogs: "socketLogs",
+  sockets: "sockets",
+  terms_of_service: "terms_of_service",
+  terms_of_service_compliance: "terms_of_service_compliance",
+  users: "users",
+  wifiErrors: "wifiErrors",
+};
 
 const array = (records) => Object.values(records);
 const db = fsb.app.app.firestore();
@@ -42,31 +40,31 @@ async function run() {
     let orgs = await fsb.exportCustom(["orgs"]);
     let orgList = array(orgs.orgs);
     for (let orgIndex = 0; orgIndex < orgList.length; orgIndex++) {
-      // for (let orgIndex = 0; orgIndex < 1; orgIndex++) {
       const orgObject = orgList[orgIndex];
-      const newOrgObject = await database.collection("orgs").insertOne(orgObject);
+      const newOrgObject = await database.collection(cNames.orgs).insertOne(orgObject);
+      const newOrgId = newOrgObject.insertedId.toString();
 
       // Add users
-      const userSnap = await _j(db.collection("users").where("orgId", "==", orgObject.id).get());
+      const userSnap = await _j(db.collection(cNames.users).where("orgId", "==", orgObject.id).get());
       for (let userIndex = 0; userIndex < userSnap.length; userIndex++) {
         const userObject = userSnap[userIndex];
         if (userObject != undefined) {
-          userObject.orgId = newOrgObject.insertedId.toString();
-          await database.collection("users").insertOne(userObject);
+          userObject.orgId = newOrgId;
+          await database.collection(cNames.users).insertOne(userObject);
+        }
+      }
+
+      // Add org members
+      // TODO: UPDATE UID based on mongo ID
+      const membersSnap = await _j(db.collection(cNames.org_members).where("orgId", "==", orgObject.id).get());
+      for (let memberIndex = 0; memberIndex < membersSnap.length; memberIndex++) {
+        const memberObject = membersSnap[memberIndex];
+        if (memberObject != undefined) {
+          memberObject.orgId = newOrgId;
+          await database.collection(cNames.org_members).insertOne(memberObject);
         }
       }
     }
-
-    // let filteredList = list;
-    // for (let i = 0; i < filteredList.length; i++) {
-    //   let collectionName = filteredList[i];
-    //   let records = await fsb.exportCustom([collectionName]);
-    //   const items = Object.values(records[collectionName]);
-    //   const table = database.collection(collectionName);
-    //   const options = { ordered: true };
-    //   const result = await table.insertMany(items, options);
-    //   console.log(`${result.insertedCount} documents were inserted`);
-    // }
   } finally {
     await client.close();
   }
